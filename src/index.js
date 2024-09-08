f6b9131830fac4a112b01dbf3d72bdbb57f78071
Pedro Perez <plperezpiris@gmail.com>
2024-09-08 23:48:59 +0200

    done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
+let timerNode = document.querySelector("#time")
 
 
 // ITERATION 1: Add event listener to the start button
@@ -17,9 +18,26 @@ function startCountdown() {
 
 
   // Your code goes here ...
+
+   timer = setInterval(() =>{
+  remainingTime --;
+  timerNode.innerText = `${remainingTime}`
+  document.getElementById("start-btn").disabled = true
+
+  if(remainingTime === 0){
+    clearInterval(timer)
+    showToast()
+  }
+
+  },1000) 
+
 }
 
+let startButtonNode = document.querySelector("#start-btn")
+startButtonNode.addEventListener("click",() =>{
+startCountdown()
 
+})
 
 
 // ITERATION 3: Show Toast
@@ -27,12 +45,18 @@ function showToast(message) {
   console.log("showToast called!");
 
   // Your code goes here ...
+MessageNode = document.querySelector(".toast")
 
-
+MessageNode.style.visibility = "visible"
 
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
 
-}
+} closeToastNode = document.querySelector("#close-toast")
+
+closeToastNode.addEventListener("click",() =>{
+  MessageNode.style.visibility = "hidden"
+
+})
